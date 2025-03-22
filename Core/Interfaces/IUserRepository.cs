19d10033d86bbaad20843a9a236ebbcd9c39ba9f
Lucas Isabel Ferreira <lucasestudo2020@gmail.com>
2025-03-22 13:21:32 -0300

    Create base for memory repositorie

---
+++ Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    interface IUserRepository
+    {
+        Task<IEnumerable<UserModel>> GetUsersAsync();
+        Task<UserModel> GetUserByIdAsync(int id);
+        Task<UserModel> GetUserByEmailAsync(string email);
+        Task<UserModel> GetUserByUserNameAsync(string userName);
+        Task<UserModel> AddUserAsync(UserModel user);
+        Task<UserModel> UpdateUserAsync(UserModel user);
+        Task<UserModel> DeleteUserAsync(int id);
+    }
+}
---
+++ Core/Models/UserModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class UserModel : BaseClass
+    {
+        private string Username { get; set; }
+        private string Email { get; set; }
+        private string Password { get; set; }
+
+        public UserModel(string username, string email, string password)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+    }
+}
